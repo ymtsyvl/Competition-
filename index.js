190139077e7df34d24d2378d9b9d23ccaa774555
LuzieH <luziehelfmann@gmail.com>
2016-07-24 09:58:14 +0200

    added jquery code to change colour schemes of page
    there are now two colour schemes of the page: black-green or pink-green.
    The buttons on the bottom navigation bar change the colour.

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("li#two").click(function(){
+        $("#contacts").css({
+            "background-color": "#A7FFEB",
+        });
+        $(".navbar ul").css({
+            "background-color": "black",
+        });
+        $(".social ul").css({
+            "background-color": "black",
+        });
+   });
+  });
+  $(document).ready(function(){
+    $("li#one").click(function(){
+        $("#contacts").css({
+            "background-color": "#ff9999",
+        });
+        $(".navbar ul").css({
+            "background-color": "#ff9999",
+        });
+        $(".social ul").css({
+            "background-color": "#ff9999",
+        });
+   });
+  });
